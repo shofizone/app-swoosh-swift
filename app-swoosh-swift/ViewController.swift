19695cbdae0c446811ec79e2db414fc33058cb57
shofi <shofizone@gmail.com>
2020-03-14 00:01:37 +0600

    Initial Commit

---
+++ app-swoosh-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh-swift
+//
+//  Created by Shofi on 14/3/20.
+//  Copyright © 2020 Shofi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app-swoosh-swiftTests/app_swoosh_swiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  app_swoosh_swiftTests.swift
+//  app-swoosh-swiftTests
+//
+//  Created by Shofi on 14/3/20.
+//  Copyright © 2020 Shofi. All rights reserved.
+//
+
+import XCTest
+@testable import app_swoosh_swift
+
+class app_swoosh_swiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
