76e81e0ab743e848fe02ac9734fd4689ace4287f
shofi <shofizone@gmail.com>
2020-03-14 23:53:30 +0600

    League and Skill screen added !

---
+++ app-swoosh-swift/ViewController/LeagueVC.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueVC.swift
+//  app-swoosh-swift
+//
+//  Created by Shofi on 14/3/20.
+//  Copyright © 2020 Shofi. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindFromSkillVC(unwind: UIStoryboardSegue){}
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ app-swoosh-swift/ViewController/SkillVC.swift
@@ -0,0 +1,23 @@
+//
+//  SkillVC.swift
+//  app-swoosh-swift
+//
+//  Created by Shofi on 14/3/20.
+//  Copyright © 2020 Shofi. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+        @IBAction func unwindFromSkillVC(unwind: UIStoryboardSegue){}
+
+
+
+}
