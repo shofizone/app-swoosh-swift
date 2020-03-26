c16588a083fce308a2dc5d0602322d910fe82235
shofi <shofizone@gmail.com>
2020-03-27 00:17:04 +0600

    league vc logic added

---
+++ app-swoosh-swift/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh-swift
+//
+//  Created by Shofi on 26/3/20.
+//  Copyright © 2020 Shofi. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLaegue: String?
+    var selectedSkilLevel: String?
+    
+}
--- app-swoosh-swift/ViewController/LeagueVC.swift
+++ app-swoosh-swift/ViewController/LeagueVC.swift
@@ -9,22 +9,44 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    var player:Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
     
-    @IBAction func unwindFromSkillVC(unwind: UIStoryboardSegue){}
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
-    */
-
+    
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+           selectLeadueType(leagueType:"mens")
+     
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+           selectLeadueType(leagueType:"womens")
+    }
+    
+    
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeadueType(leagueType:"code")
+    }
+    
+    
+    private func selectLeadueType(leagueType:String){
+        player.desiredLaegue = leagueType
+           nextBtn.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC{
+            skillVC.player = player
+        }
+    }
 }
